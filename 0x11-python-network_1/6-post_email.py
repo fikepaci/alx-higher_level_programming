61052fee4c91cf7007b25030fc81a1377ed6e226
fikepaci <pacifiquemachine@gmail.com>
2021-04-11 14:02:55 +0200

    add file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,14 +3,14 @@
 
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
-  """
-  import sys
-  import requests
+"""
+import sys
+import requests
 
 
-  if __name__ == "__main__":
-      url = sys.argv[1]
-      value = {"email": sys.argv[2]}
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-      r = requests.post(url, data=value)
-      print(r.text)
+    r = requests.post(url, data=value)
+    print(r.text)
